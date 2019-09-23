7f86651ff1c6b8ca00a32a6323453eb3f5b318d6
Chilok20 <2076852@talnet.nl>
2019-09-23 15:58:30 +0200

    Uitwerking van de exercise

---
+++ includes/index.php
@@ -0,0 +1,21 @@
+<?php
+    include_once 'includes/
+        dbh.inc.php';
+        ?>
+
+
+    <!DOCTYPE html>
+    <html>
+    <head>
+    	<title></title>
+    </head>
+    <body>
+<?php
+    $sql =  "SELECT * FROM users'";
+    ?>
+
+
+
+
+    </body>
+    </html>
